40477aa34a9b617ca037b312008a72073bcb81ed
roger NDABA <rmdaba@c4r17s11.wethinkcode.co.za>
2019-08-21 15:13:33 +0200

    simple printf norme

--- libft/srcs/simple_printf.c
+++ libft/srcs/simple_printf.c
@@ -6,7 +6,7 @@
 /*   By: rmdaba <rogerndaba@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/21 14:07:12 by rmdaba            #+#    #+#             */
-/*   Updated: 2019/08/21 15:02:44 by rmdaba           ###   ########.fr       */
+/*   Updated: 2019/08/21 15:11:03 by rmdaba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 char	*convert(unsigned int num, int base)
 {
-	static char	rep[17];
-	static char	buffer[50];
-	char		*ptr;
+	static char		rep[17];
+	static char		buffer[50];
+	char			*ptr;
 
 	ft_strcpy(rep, "0123456789ABCDEF");
 	ptr = &buffer[49];
 	*ptr = '\0';
 	while (num != 0)
 	{
-		*--ptr = rep[num%base];
+		*--ptr = rep[num % base];
 		num /= base;
 	}
-	return(ptr);
+	return (ptr);
 }
 
 void	proc(unsigned int *i, va_list arg, char c, char **s)
@@ -38,8 +38,8 @@ void	proc(unsigned int *i, va_list arg, char c, char **s)
 	}
 	else if (c == 'd')
 	{
-		 *i = va_arg(arg,int);
-		if ( *i < 0)
+		*i = va_arg(arg, int);
+		if (*i < 0)
 		{
 			*i = -*i;
 			ft_putchar('-');
@@ -53,13 +53,13 @@ void	proc(unsigned int *i, va_list arg, char c, char **s)
 	}
 }
 
-void	simple_printf(char* format, ...)
+void	simple_printf(char *format, ...)
 {
-	char			*traverse;
 	unsigned int	i;
+	char			*traverse;
 	char			*s;
+	va_list			arg;
 
-	va_list arg;
 	va_start(arg, format);
 	traverse = format;
 	while (*traverse != '\0')
