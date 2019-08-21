0ee89315941290a29951ed939e45fefa2a0d268f
rogerndaba <rogerndaba@gmail.com>
2019-08-21 22:39:06 +0200

    last push, wednesday

--- main.c
+++ main.c
@@ -5,5 +5,11 @@ int main(int ac, char **av)
 	t_checker	t_c;
 
 	normalize_argv(&ac, &av);
+	t_c.size_a = ac;
+	t_c.size_b = 0;
+	build_stacks(&t_c, &av, ac);
+	free_double_arr((void ***)&av);
+	free(t_c.s_a);
+	free(t_c.s_b);
 	return 0;
 }
