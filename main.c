1ecc3eb5fcafb2e86710c1b923f3ac13d8d79282
rogerndaba <rogerndaba@gmail.com>
2019-08-28 21:37:50 +0200

    ccccc

--- main.c
+++ main.c
@@ -122,7 +122,7 @@ void	ps_proccess1(t_checker *t_c, int *new_arr, int size)
 				// 	printf(" %d", new_arr[i]);
 				// }
 
-				printf("\n--------------> %d\n", close);ff
+				printf("\n--------------> %d\n", close);
 				// exit(1);
 				// simple_print(t_c);
 
